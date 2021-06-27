645f870338cb19004012ba77403f9bede1e146ab
tajimakaito <Donan.fugarasa4792@icloud.com>
2021-06-27 11:31:09 +0900

    マイグレーションを実行

--- db/migrate/20210627022521_create_foods.rb
+++ db/migrate/20210627022521_create_foods.rb
@@ -1,7 +1,7 @@
 class CreateFoods < ActiveRecord::Migration[6.1]
   def change
     create_table :foods do |t|
-      t.string :name
+      t.string :name, null: false
       t.text :comment
       t.references :user, null: false, foreign_key: true
 
