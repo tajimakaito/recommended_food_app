fd72d60f8adb60d4b542e07b64a42b287f9acf97
tajimakaito <Donan.fugarasa4792@icloud.com>
2021-06-27 22:25:41 +0900

    削除機能実装

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,4 +1,6 @@
 class FoodsController < ApplicationController
+  before_action :set_food, only: %i[edit update destroy]
+
   def index
     @foods = Food.includes(:user).order(:created_at)
   end
@@ -20,11 +22,19 @@ class FoodsController < ApplicationController
 
   def update; end
 
-  def destroy; end
+  def destroy
+    @food.destroy!
+    redirect_to root_path
+  end
 
   private
 
   def food_params
     params.require(:food).permit(:name, :comment)
+  end
+
+  def set_food
+    @food = current_user.foods.find_by(id: params[:id])
+    redirect_to root_path, alert: "権限がありません" if @food.nil?
   end
 end
