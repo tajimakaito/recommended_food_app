86ed4088c2c5f8d735eb932db5f73d3a819231ab
tajimakaito <Donan.fugarasa4792@icloud.com>
2021-07-02 17:03:30 +0900

    画像ファイルサイズのバリデーションを設定

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -21,6 +21,10 @@ class ImageUploader < CarrierWave::Uploader::Base
   #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
   # end
 
+  def size_range
+    0..5.megabytes
+  end
+
   # Process files as they are uploaded:
   # process scale: [200, 300]
   #
