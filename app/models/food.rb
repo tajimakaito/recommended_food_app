04e05dfd25dd5d4e29f3e80f21316b88d68afc47
tajimakaito <Donan.fugarasa4792@icloud.com>
2021-07-26 16:49:00 +0900

    UserモデルとFoodモデルの多対多の関連付け

--- app/models/food.rb
+++ app/models/food.rb
@@ -2,5 +2,6 @@ class Food < ApplicationRecord
   mount_uploader :image, ImageUploader
   belongs_to :user
   has_many :likes, dependent: :destroy
+  has_many :liked_users, through: :likes, source: :user
   validates :name, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_many :foods, dependent: :destroy
   has_many :likes, dependent: :destroy
+  has_many :liked_foods, through: :likes, source: :food
   devise :database_authenticatable, :registerable,
          :rememberable, :validatable
 end
