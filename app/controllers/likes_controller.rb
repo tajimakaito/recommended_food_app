92514c6e14b099cf65cb59cb85fd35b3dc4c4c7e
tajimakaito <Donan.fugarasa4792@icloud.com>
2021-07-27 09:55:55 +0900

    いいね機能の非同期処理を実装

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,11 +1,11 @@
 class LikesController < ApplicationController
   def create
+    @food = Food.find(params[:food_id])
     current_user.likes.create!(food_id: params[:food_id])
-    redirect_back(fallback_location: root_path)
   end
 
   def destroy
+    @food = Food.find(params[:food_id])
     current_user.likes.find_by(food_id: params[:food_id]).destroy!
-    redirect_back(fallback_location: root_path)
   end
 end
